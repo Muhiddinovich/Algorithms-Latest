f7ae749d439cfb47305008b7310926ee0d8af17a
Muhiddinovich <mullagoziev@gmail.com>
2024-11-01 05:25:59 -0400

    Quick Union finished

--- union-find/src/main/java/com/fred/unionfind/union_find/App.java
+++ union-find/src/main/java/com/fred/unionfind/union_find/App.java
@@ -4,31 +4,47 @@ package com.fred.unionfind.union_find;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	
-    	//Quck Find
-        QuickFindUF quickFindUF = new QuickFindUF(10);
-        
-        quickFindUF.union(4, 3);
-        quickFindUF.union(3, 8);
-        quickFindUF.union(6, 5);
-        
-        quickFindUF.union(9, 4);
-        quickFindUF.union(2, 1);
-        quickFindUF.union(5, 0);
-        quickFindUF.union(7, 2);
-        quickFindUF.union(6, 1);
-        
+public class App {
+	public static void main(String[] args) {
+
+		// Quck Find
+		QuickFindUF quickFindUF = new QuickFindUF(10);
+
+		quickFindUF.union(4, 3);
+		quickFindUF.union(3, 8);
+		quickFindUF.union(6, 5);
+
+		quickFindUF.union(9, 4);
+		quickFindUF.union(2, 1);
+		quickFindUF.union(5, 0);
+		quickFindUF.union(7, 2);
+		quickFindUF.union(6, 1);
+
 //        System.out.println(quickFindUF.toString());
 //        System.out.println(quickFindUF.connected(4, 9));
-        
-        // Quick Union
-        
-       QuickUnion quickUnion = new QuickUnion(10);
-    	
-    	
-    }
+
+		// Quick Union
+
+		QuickUnion quickUnion = new QuickUnion(10);
+		System.out.println(quickUnion.toString());
+		quickUnion.union(4, 3);
+		System.out.println(quickUnion.toString());
+		quickUnion.union(3, 8);
+		System.out.println(quickUnion.toString());
+		quickUnion.union(6, 5);
+		System.out.println(quickUnion.toString());
+		quickUnion.union(9, 4);
+		System.out.println(quickUnion.toString());
+		quickUnion.union(2, 1);
+		System.out.println(quickUnion.toString());
+		quickUnion.union(5, 0);
+		System.out.println(quickUnion.toString());
+		quickUnion.union(7, 2);
+		System.out.println(quickUnion.toString());
+		quickUnion.union(6, 1);
+		System.out.println(quickUnion.toString());
+		quickUnion.union(7, 3);
+		System.out.println(quickUnion.toString());
+
+	}
 }
